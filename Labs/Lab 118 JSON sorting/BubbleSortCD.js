ec71b3ea28b3a77b90752549d58a2e3112f90c57
795311 <795311@seq.org>
2019-01-31 08:47:41 -0800

    made changes

--- Labs/Lab 118 JSON sorting/BubbleSortCD.js
+++ Labs/Lab 118 JSON sorting/BubbleSortCD.js
@@ -1,6 +1,9 @@
 var countrydata = [];
 function preload(){
   countrydata = loadJSON("population.json");
+  var = "total"
+  var = "males"
+  var = "age"
 }
 
 function setup() {
@@ -14,7 +17,35 @@ function draw(){
 
 }
 
-function bubbleSort(countrydata){
+function bubbleSort("total"){
+  var length = countrydata.length;
+  for(var i = 0; i < length; i--){
+      for(var j = 0; i < (length - i - 1); i++){
+        var a = countrydata[j].females;
+        var b = countrydata[j+1].females;
+        if (a > b) {
+          var temp = a;
+          a = b;
+          a = temp;
+        }
+      }
+  }
+}
+function bubbleSort("age"){
+  var length = countrydata.length;
+  for(var i = 0; i < length; i--){
+      for(var j = 0; i < (length - i - 1); i++){
+        var a = countrydata[j].females;
+        var b = countrydata[j+1].females;
+        if (a > b) {
+          var temp = a;
+          a = b;
+          a = temp;
+        }
+      }
+  }
+}
+function bubbleSort("males"){
   var length = countrydata.length;
   for(var i = 0; i < length; i--){
       for(var j = 0; i < (length - i - 1); i++){
